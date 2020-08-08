0a4ec41db096f2cec75bebbfce836d3d6346ad87
leokodish <lkkodish@gmail.com>
2020-08-07 18:03:35 -1000

    deleted old text files and bash scripts

--- exectest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#curl http://www.songlyrics.com/nwa/straight-outta-compton-lyrics/ > lyrics.txt
-#sed -n '/<p id="songLyricsDiv"/,$ p' lyrics.txt > temp.txt
-#sed '/<\/p>/,$d' temp.txt > temp2.txt
-#sed 's/<br \/>//' temp2.txt > temp3.txt
-#sed '1s/[^>]*>//' temp3.txt > temp4.txt
-#cat temp4.txt
